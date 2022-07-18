c006613299bc5a4ceb78557f1d17e7e1c8f25f14
vveewwee <vgeorgakakou@gmail.com>
2022-07-18 21:40:17 +0200

    error with saving and checkin out

---
+++ src/components/PlantItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import CareScale from './CareScale'
+import '../styles/PlantItem.css'
+
+export default function PlantItem({name, cover, id, light, water}) {
+  return (
+    <li key={`${id}-${name}-${cover}`} className='lmj-plant-item'>
+        <img key={`${name}-${id}`} className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
+        {name}
+        <div>
+            <CareScale careType='water' scaleValue={water} />
+            <CareScale careType='light' scaleValue={light} />
+        </div>
+    </li>
+)
+}
